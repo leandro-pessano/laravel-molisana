41939901148b4c31182f256482005b628b5a3404
Leandro <thesquallrock@gmail.com>
2021-03-10 17:53:25 +0100

    home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+  <main class="home">
+    <img src=" {{asset('img/molisana-home.jpg')}}" alt="">
+  </main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})
+->name('home');
+
+Route::get('/prodotti', function () {
+    return view('products');
+})
+->name('products');
+
+Route::get('/news', function () {
+    return view('news');
+})
+->name('news');
